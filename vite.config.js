e2c0dc010dcb775872a8c979984a2b5ae10ebe2f
BozidarVukovic <bozidar.vukovic@yahoo.com>
2026-04-19 16:20:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // react-snap werkt beter zonder inline assets
-    assetsInlineLimit: 0,
-  },
 })
