9fc5f3bea4505cd131a57fdd1890e80601a132e7
BozidarVukovic <bozidar.vukovic@yahoo.com>
2026-05-08 18:00:38 +0000

    Herstel Firebase webconfig

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Centrale Firebase-configuratie voor Mijn Teamkompas.
-// Hierdoor wordt Firebase maar één keer geïnitialiseerd en voorkom je dubbele configuratie.
 const firebaseConfig = {
   apiKey: "AIzaSyDyl6gj1LmOZ-1Mcin1JnfkkZg82c2Jtz0",
   authDomain: "mijn-teamkompas-6de84.firebaseapp.com",
@@ -11,6 +10,7 @@ const firebaseConfig = {
   storageBucket: "mijn-teamkompas-6de84.firebasestorage.app",
   messagingSenderId: "820620515571",
   appId: "1:820620515571:web:86a4e792eebe4c7cf03f86",
+  measurementId: "G-9DSYN4LZ94",
 };
 
 const app = initializeApp(firebaseConfig);
