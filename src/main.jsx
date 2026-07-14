1b8a37f7335a602c22d1ad77f2370b44a65f28e6
BozidarVukovic <bozidar.vukovic@yahoo.com>
2026-07-15 00:07:41 +0200

    Activeer overzichtelijke navigatie met dropdowns

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
+import OrganizedNavigation from "./components/shared/OrganizedNavigation";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <OrganizedNavigation />
       <App />
       <Analytics />
     </BrowserRouter>
